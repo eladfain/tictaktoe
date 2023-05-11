0047ada1ed71a1764c66b367090910d955dd9a11
eladfain <62603008+eladfain@users.noreply.github.com>
2023-05-11 16:47:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+let xTurn=true,
+gameOn=true;
+const winOBJ={
+    "c1":[["c1","c2","c3"],["c1","c4","c7"],["c1","c5","c9"]],
+    "c2":[["c1","c2","c3"],["c2","c5","c8"]],
+    "c3":[["c1","c2","c3"],["c3","c6","c9"],["c3","c5","c7"]],
+    "c4":[["c4","c5","c6"],["c1","c4","c7"]],
+    "c5":[["c4","c5","c6"],["c2","c5","c8"]],
+    "c6":[["c4","c5","c6"],["c3","c6","c9"]],
+    "c7":[["c7","c8","c9"],["c1","c4","c7"],["c3","c5","c7"]],
+    "c8":[["c7","c8","c9"],["c2","c5","c8"]],
+    "c9":[["c7","c8","c9"],["c3","c6","c9"],["c1","c5","c9"]]
+}
+function init(){
+    const cells=document.querySelectorAll(".cell");
+    cells.forEach(cell=>addEventListener('click',(e)=>{
+        if(gameOn){
+            const id=e.target.id;
+            if(!hasValue(id)){
+                const turn=xTurn?"X":"O";
+                new Audio(`./resource/${turn}.wav`).play();
+                document.getElementById(id).innerHTML=turn;
+                const isWin=checkWin(id);
+                if(isWin){
+                    win(isWin)
+                }
+                else{
+                    xTurn=!xTurn;
+                }
+            }
+        }
+       
+    }))
+}
+function hasValue(cellId){
+    const cell=document.getElementById(cellId);
+    return cell.textContent.length;
+}
+function checkWin(cellId){
+    const checkArr=winOBJ[cellId],
+    compare=xTurn?"X":"O";
+    for(let i=0;i<checkArr.length;i++){
+        let foundWin=true;
+        for(let j=0;j<checkArr[i].length;j++){
+            const checkCellValue=document.getElementById(checkArr[i][j]).textContent;
+            if(checkCellValue!==compare){
+                foundWin=false;
+            }
+        }
+        if(foundWin){
+            return checkArr[i];
+        }
+    }
+    return false;
+}
+function win(winArr){
+    winArr.forEach(cell=>{
+        document.getElementById(cell).classList.add("win")
+    })
+    gameOn=false;
+    new Audio("./resource/win.wav").play();
+}
+init();
